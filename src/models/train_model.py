c898c269e307e711984019a3e343d5f1e500f6aa
udaylunawat <udaylunawat@gmail.com>
2020-09-18 18:15:25 +0530

    Data Generator

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,48 +1,22 @@
-from tensorflow.keras.utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
+import joblib 
 from keras.utils import np_utils
-from sklearn.model_selection import train_test_split
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix, classification_report
 
-from keras.utils import np_utils
-
-from tensorflow.keras.models import Model
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import VGG16
-from tensorflow.keras.layers import Input, Dense, Flatten, MaxPooling2D, GlobalAveragePooling2D, Dropout, BatchNormalization, Conv2D, InputLayer
-from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.layers import Input, Dense, Flatten, Dropout
 
-encoder = LabelEncoder()
-encoded_labels = encoder.fit_transform(y)
-y_encoded = np_utils.to_categorical(encoded_labels)
-
-
-X_train, X_cvtest, y_train, y_cvtest = train_test_split(X, y_encoded, test_size=0.40, stratify=y_encoded, random_state=42)
-X_cv, X_test, y_cv, y_test = train_test_split(X_cvtest, y_cvtest, test_size=0.50, stratify=y_cvtest, random_state=42)
-
-del X
-del y_encoded
-del encoded_labels
-
-
-BATCH_SIZE = 64
-TRAIN_LENGTH = len(X_train)
-VAL_LENGTH = len(X_cv)
-STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
-SPE_VAL = VAL_LENGTH // BATCH_SIZE
-IMG_SIZE = 224
-LR = 0.0001
-EPOCHS = 50
-
-datagen = ImageDataGenerator(
-        rotation_range=15,
-        fill_mode="nearest")
+def target_preprocessing(y)
+    encoder = LabelEncoder()
+    encoded_labels = encoder.fit_transform(y)
+    y_encoded = np_utils.to_categorical(encoded_labels)
+    return y_encoded
 
 def VGG16_model():
     base = VGG16(include_top=False, weights='imagenet', input_shape=(IMG_SIZE, IMG_SIZE, 3))
@@ -57,6 +31,44 @@ def VGG16_model():
     
     return model
 
+y_encoded = target_preprocessing(y)
+X_train, X_cvtest, y_train, y_cvtest = train_test_split(X, y_encoded, test_size=0.40, stratify=y_encoded, random_state=42)
+X_cv, X_test, y_cv, y_test = train_test_split(X_cvtest, y_cvtest, test_size=0.50, stratify=y_cvtest, random_state=42)
+
+
+BATCH_SIZE = 64
+IMG_SIZE = 224
+LR = 0.0001
+EPOCHS = 20
+
+datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.25,
+                             rotation_range=15,
+                             fill_mode="nearest")
+
+
+train_generator = datagen.flow_from_dataframe(
+    dataframe = data, directory = None,
+    x_col = "path", y_col = "label",
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    subset='training',
+    class_mode="categorical"
+    )
+
+validation_generator = datagen.flow_from_dataframe(
+    dataframe = data, directory = None,
+    x_col = "path", y_col = "label",
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    subset='validation',
+    class_mode="categorical"
+    )
+
+TRAIN_SPE = train_generator.samples//BATCH_SIZE
+VAL_SPE = validation_generator.samples//BATCH_SIZE
+
 model = Sequential()
 model.add(VGG16_model())
 model.add(Dropout(0.3))
@@ -65,7 +77,7 @@ model.add(Dropout(0.3))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(3, activation='sigmoid'))
 
-model.summary()
+# model.summary()
 
 tf.keras.backend.clear_session()
 
@@ -74,21 +86,25 @@ optimizer = Adam(lr = LR, decay = LR/EPOCHS)
 
 model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-earlystop = EarlyStopping(monitor='val_accuracy', min_delta = 0.002, patience = 5 ,mode = 'auto', verbose = 1)
+earlystop = EarlyStopping(monitor='val_accuracy', min_delta = 0.001, patience = 10 ,mode = 'auto', verbose = 1)
 
-ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max'
-checkpointer = ModelCheckpoint(filepath = '/content/model-{epoch:03d}.h5', 
-                               monitor = 'val_accuracy', verbose = 1, save_best_only = False, mode ='auto')
+checkpointer = ModelCheckpoint(filepath = 'output/models/snapshots/model-{epoch:03d}-{val_accuracy:03f}.h5', 
+                               monitor = 'val_accuracy', verbose = 1, save_best_only = True, mode ='auto')
 
 callbacks_list = [earlystop, checkpointer]
 
-history = model.fit(datagen.flow(X_train, y_train, batch_size=BATCH_SIZE),
-                                    steps_per_epoch=STEPS_PER_EPOCH,
-                                    validation_data=(X_cv, y_cv),
-                                    validation_steps=SPE_VAL,
-                                    epochs=EPOCHS,
-                                    verbose=1, callbacks = callbacks_list)
+history = model.fit(train_generator, 
+                    batch_size=BATCH_SIZE,
+                    steps_per_epoch=TRAIN_SPE,
+                    validation_data=validation_generator,
+                    validation_steps=VAL_SPE,
+                    epochs=EPOCHS,
+                    verbose=1, callbacks = callbacks_list)
 
 print("Training Completed")
-print("Saving Model as base_model_covid.h5 in /output/models directory")
-model.save('/content/output/models/base_model_covid.h5')
+print("Saving Model as model.h5 in /output/models directory")
+model.save('/content/output/models/model.h5')
+  
+# Save the model as a pickle in a file
+print("Saving model metrics as output/history.pkl")
+joblib.dump(history.history, 'output/history.pkl') 
