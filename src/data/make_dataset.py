1489bbb04abe3a3771967c39769bfe59b8d57d6f
udaylunawat <udaylunawat@gmail.com>
2020-09-19 07:47:04 +0530

    update: make_dataset saved processed data.csv

---
+++ src/data/make_dataset.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import pandas as pd
+from src.config import rapid-api-key, DATA_DIR, PROCESSED_DATA_PATH
+
+def main():
+    imagePaths = []
+    for dirname, _, filenames in os.walk(DATA_DIR):
+        for filename in filenames:
+            if (filename[-3:] == 'png'):
+                imagePaths.append(os.path.join(dirname, filename))
+
+    path = []
+    label = []
+    for image in imagePaths:
+        path.append(image)
+        label.append(image.split('/')[3])
+
+    data = pd.DataFrame({'path':path,'label':label})
+    data.to_csv(PROCESSED_DATA_PATH, index=False)
+
+def live_data(key):
+    # https://rapidapi.com/astsiatsko/api/coronavirus-monitor
+    url = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php"
+
+    headers = {
+        'x-rapidapi-host': "coronavirus-monitor.p.rapidapi.com",
+        'x-rapidapi-key': rapid-api-key
+        }
+
+    response = requests.request("GET", url, headers=headers)
+    return response
+
+if __name__ == '__main__':
+    main()
